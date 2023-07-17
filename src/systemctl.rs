045197e8b26b73c25dc1749c0e1cfed88573eda7
Banyc <36535895+Banyc@users.noreply.github.com>
2023-07-17 19:28:02 +0800

    fix: systemd service file

--- src/systemctl.rs
+++ src/systemctl.rs
@@ -1,6 +1,7 @@
-use std::sync::Arc;
+use std::{io::Write, sync::Arc};
 
 use clap::Args;
+use tempfile::NamedTempFile;
 use xshell::{cmd, Shell};
 
 /// Overwrite systemd service file.
@@ -12,12 +13,12 @@ pub struct SystemctlArgs {
     pub server_path: Arc<str>,
     /// e.g.: `example`
     pub binary_name: Arc<str>,
-    /// e.g.: `"-a -b c"`
-    pub binary_args: Arc<str>,
     /// e.g.: `user`
     pub user: Arc<str>,
     /// e.g.: `group`
     pub group: Arc<str>,
+    /// e.g.: `-- -a -b c`
+    pub binary_args: Vec<Arc<str>>,
 }
 
 pub fn systemctl(args: SystemctlArgs) -> Result<(), Box<dyn std::error::Error>> {
@@ -29,19 +30,23 @@ pub fn systemctl(args: SystemctlArgs) -> Result<(), Box<dyn std::error::Error>> 
         &args.group,
     );
     let text = std::dbg!(text);
+    let mut temp_file = NamedTempFile::new()?;
+    temp_file.write_all(text.as_bytes())?;
 
     let binary_name = args.binary_name;
     let service_file_path = format!("/etc/systemd/user/{binary_name}.service");
-    let write_file_cmd = format!(
-        "rm -f {service_file_path}
-        touch {service_file_path}
-        echo '{text}' > {service_file_path}
-        systemctl enable {service_file_path}"
-    );
-
-    let sh = Shell::new()?;
     let server_ssh = args.server_ssh.as_ref();
-    cmd!(sh, "ssh -q -T {server_ssh} {write_file_cmd}").run()?;
+    let sh = Shell::new()?;
+
+    // Remove service file
+    cmd!(sh, "ssh {server_ssh} rm -f {service_file_path}").run()?;
+
+    // Transfer service file
+    let temp_file_path = temp_file.path();
+    cmd!(sh, "scp {temp_file_path} {server_ssh}:{service_file_path}").run()?;
+
+    // Enable service
+    cmd!(sh, "ssh {server_ssh} systemctl enable {service_file_path}").run()?;
 
     Ok(())
 }
@@ -49,17 +54,24 @@ pub fn systemctl(args: SystemctlArgs) -> Result<(), Box<dyn std::error::Error>> 
 fn service_text(
     server_path: &str,
     binary_name: &str,
-    binary_args: &str,
+    binary_args: &[Arc<str>],
     user: &str,
     group: &str,
 ) -> String {
+    let mut args = String::new();
+    for arg in binary_args {
+        args.push('\"');
+        args.push_str(arg);
+        args.push('\"');
+        args.push(' ');
+    }
     format!(
         "[Unit]
 Description={binary_name}
 After=network.target
 
 [Service]
-ExecStart=/bin/sh -c {server_path}/{binary_name} {binary_args}
+ExecStart=/bin/sh -c \'{server_path}/{binary_name} {args}\'
 User={user}
 Group={group}
 
